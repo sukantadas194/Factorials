c9ed89118ed32cee7e862c7f003318ab21f755ba
Sukanta Das <83443864+sukantadas194@users.noreply.github.com>
2021-07-10 21:37:21 +0530

    Update Write a code to find a Factorial of a given number using Recursion..py

--- Write a code to find a Factorial of a given number using Recursion..py
+++ Write a code to find a Factorial of a given number using Recursion..py
@@ -1,6 +1,6 @@
 
 # Write a code to find a Factorial of a given number using Recursion.
-# Recursion is function which can call itself.
+# Recursion is a function which can call itself.
 # Factorial example: 5! = 5*4*3*2*1 = 120
 # Remember -> 0! = 1 & n! = n*(n-1)*(n-2)*(n-3)*(n-4)...
 
@@ -33,13 +33,3 @@ def error():
 error()
 
 
-
-
-
-#result = Factorial(n)
-#print(n,"! =",result)
-
-    # except:
-    #     print("Wrong Input")
-    #     print("Start again..")
-    #     Factorial(n)
